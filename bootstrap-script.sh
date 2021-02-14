79b62be1de4dc09a9d9cd4eddeb903033b851c6a
josue <josuevora@gmail.com>
2021-02-13 21:57:26 -0600

    :construction_worker: First bootstrap script to install docker automatically

---
+++ bootstrap-script.sh
@@ -0,0 +1,9 @@
+#Bootstrap-script to automatize docker installation on Ubuntu20
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker ubuntu
